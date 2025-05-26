b571f2e7b351ffda4f2d0eaa56369f3acb1352ef
Vadim Zhuravlev <v.zhuravlev@andersenlab.com>
2025-05-26 04:27:54 +0200

    changed dist to build

--- vite.config.ts
+++ vite.config.ts
@@ -6,14 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-    server: {
-    proxy: {
-      '/api': {
-        target: 'https://testjob.checkport.ru', // 👈 https, даже если он редиректит
-        changeOrigin: true,
-        secure: false, // ⚠️ отключает проверку сертификата
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
+   build: {
+    outDir: 'build',
   },
 });
