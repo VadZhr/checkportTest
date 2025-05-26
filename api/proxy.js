da6699245b2bbcba29ca08f18ae22b5129404cb1
Vadim Zhuravlev <v.zhuravlev@andersenlab.com>
2025-05-26 05:16:33 +0200

    changed proxy

--- api/proxy.js
+++ api/proxy.js
@@ -1,5 +1,12 @@
 export default async function handler(req, res) {
-  const targetUrl = `http://testjob.checkport.ru${req.url?.replace('/api/proxy', '')}`;
+  const { url, ...params } = req.query;
+
+  if (!url) {
+    return res.status(400).json({ error: 'Missing url parameter' });
+  }
+
+  const queryString = new URLSearchParams(params).toString();
+  const targetUrl = `http://testjob.checkport.ru${url}${queryString ? `?${queryString}` : ''}`;
 
   try {
     const response = await fetch(targetUrl, {
@@ -8,11 +15,9 @@ export default async function handler(req, res) {
         ...req.headers,
         host: 'testjob.checkport.ru',
       },
-      body: req.method !== 'GET' && req.body ? JSON.stringify(req.body) : undefined,
     });
 
     const data = await response.text();
-
     res.status(response.status).send(data);
   } catch (error) {
     res.status(500).json({ error: 'Proxy failed', details: String(error) });
